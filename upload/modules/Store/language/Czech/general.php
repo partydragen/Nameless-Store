e0930ae6bcfa9016da4e6348201274dbf420ca4e
Fjuro <fjuro@seznam.cz>
2021-11-30 16:05:01 +0100

    Create Czech version of general.php

---
+++ upload/modules/Store/language/Czech/general.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ *  Made by Partydragen, Translated by Fjuro
+ *  https://partydragen.com/resources/resource/5-store-module/
+ *  https://partydragen.com/
+ *
+ *  License: MIT
+ *
+ *  Store module - Czech Language
+ */
+ 
+$language_version = '2.0.0-pr12';
+
+$language = array(
+    /*
+     *  General terms
+     */
+    'store' => 'Obchod',
+    'home' => 'Domů',
+    'name' => 'Název',
+    'buy' => 'Koupit',
+    'sale' => 'Sleva',
+    'price' => 'Cena',
+    'quantity' => 'Počet',
+    'product' => 'Produkt',
+    'products' => 'Produkty',
+    'no_products' => 'Nebyly nalezeny žádné produkty!',
+    'checkout' => 'Platba',
+    'purchase' => 'Zakoupit',
+    'summary' => 'Souhrn',
+    'continue' => 'Pokračovat',
+    'please_enter_username' => 'Zadejte vaše Minecraft jméno pro pokračování',
+    'logout' => 'Odhlásit se',
+    'shopping_cart' => 'Nákupní košík',
+    'total_price' => 'Celková cena:',
+    'payment_method' => 'Způsob platby',
+    'agree_t_and_c_purchase' => 'I agree to the <a href="{x}" target="_blank">terms and conditions</a> of this purchase.',
+    'x_items_for_y' => '{x} item(s) for {y}',
+);
