c40655957e12cac7dc209c8496db24bb42249a1b
Justman10000 <49990629+Justman10000@users.noreply.github.com>
2021-12-26 22:42:50 +0100

    Czech added - Not Translated!

---
+++ upload/modules/Store/language/Czech/perms.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ *  Made by Partydragen
+ *  https://partydragen.com/resources/resource/5-store-module/
+ *  https://partydragen.com/
+ *
+ *  License: MIT
+ *
+ *  Store module - Czech Language
+ */
+ 
+$language_version = '2.0.0-pr12';
+
+$language = array(
+    'staffcp_store' => 'NOT_TRANSLATED',
+    'staffcp_store_settings' => 'NOT_TRANSLATED',
+    'staffcp_store_gateways' => 'NOT_TRANSLATED',
+    'staffcp_store_products' => 'NOT_TRANSLATED',
+    'staffcp_store_payments' => 'NOT_TRANSLATED',
+    'staffcp_store_manage' => 'NOT_TRANSLATED',
+);
