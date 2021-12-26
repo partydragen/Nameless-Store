61a5f5585e368cc738193da6c1617501371f4483
Justman10000 <49990629+Justman10000@users.noreply.github.com>
2021-12-26 22:35:34 +0100

    Fixed missing strings

---
+++ upload/modules/Store/language/EnglishUK/perms.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ *  Made by Partydragen & Justman10000
+ *  https://partydragen.com/resources/resource/5-store-module/
+ *  https://partydragen.com/
+ *
+ *  License: MIT
+ *
+ *  Store module - German Language
+ */
+ 
+$language_version = '2.0.0-pr12';
+
+$language = array(
+    'staffcp_store' => 'Store',
+    'staffcp_store_settings' => 'Settings',
+    'staffcp_store_gateways' => 'Gateways',
+    'staffcp_store_products' => 'Products',
+    'staffcp_store_payments' => 'Payments',
+    'staffcp_store_manage' => 'Manage',
+);
