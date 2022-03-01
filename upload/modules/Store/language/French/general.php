579f53cfcc7401404fbaf43a3694320579d9d002
Twiz0x <78769585+Twiz0x@users.noreply.github.com>
2022-03-01 16:25:39 +0100

    Add files via upload

---
+++ upload/modules/Store/language/French/general.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ *  Made by Partydragen
+ *  https://partydragen.com/resources/resource/5-store-module/
+ *  https://partydragen.com/
+ *
+ *  License: MIT
+ *
+ *  Store module - English Language
+ */
+ 
+$language_version = '2.0.0-pr12';
+
+$language = array(
+    /*
+     *  General terms
+     */
+    'store' => 'Boutique',
+    'home' => 'Accueil',
+    'name' => 'Name',
+    'buy' => 'Acheter',
+    'sale' => 'Vente',
+    'price' => 'Prix',
+    'quantity' => 'Quantitée',
+    'product' => 'Produit',
+    'products' => 'Produits',
+    'no_products' => 'Aucun produit n"a été trouvé !',
+    'checkout' => 'Caisse',
+    'purchase' => 'Acheter',
+    'summary' => 'Sommaire',
+    'continue' => 'Continuer',
+    'options' => 'Options',
+    'please_enter_username' => 'Veuillez entrer votre pseudo minecraft pour continuer',
+    'logout' => 'Déconnecter',
+    'shopping_cart' => 'Panier',
+    'total_price' => 'Prix Total:',
+    'payment_method' => 'Moyen de Paiement',
+    'agree_t_and_c_purchase' => 'J"accepte les <a href="{x}" target="_blank">termes et conditions</a> de ce paiement.',
+    'x_items_for_y' => '{x} élément(s) pour {y}',
+    
+    'x_field_minimum_y' => '{x} doit être au minimum de {y} caractères.',
+    'x_field_maximum_y' => '{x} doit être au maximum de {y} caractères.',
+    
+    'pending_payment_text' => 'Paiement en attente de {x}',
+    'completed_payment_text' => 'Nouveau paiement de {x} qui a acheté les produits suivants {y}',
+    'refunded_payment_text' => 'Paiement remboursé de {x}',
+    'reversed_payment_text' => 'Paiement annulé de {x}',
+    'denied_payment_text' => 'Paiement refusé de {x}',
+);
