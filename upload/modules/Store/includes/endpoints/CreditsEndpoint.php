0ca8742c73ba0cc4c1c558fb19d12e5f54c6e803
Partydragen <admin@partydragen.com>
2022-07-21 00:30:52 +0200

    Add credits endpoint

---
+++ upload/modules/Store/includes/endpoints/CreditsEndpoint.php
@@ -0,0 +1,21 @@
+<?php
+class CreditsEndpoint extends KeyAuthEndpoint {
+
+    public function __construct() {
+        $this->_route = 'users/{user}/credits';
+        $this->_module = 'Store';
+        $this->_description = 'Check user credits';
+        $this->_method = 'GET';
+    }
+
+    public function execute(Nameless2API $api, User $user): void {
+        $customer = new Customer($user);
+        $customer->addCredits($credits);
+
+        $api->returnArray([
+            'customer_id' => (int) $customer->data()->id,
+            'cents' => (int) $customer->data()->cents,
+            'credits' => (double) $customer->data()->cents / 100
+        ]);
+    }
+}
