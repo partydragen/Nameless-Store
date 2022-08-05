919c997ee256feda2ef0f753bb1f9876b7564b81
Partydragen <admin@partydragen.com>
2022-08-05 18:48:07 +0200

    Release v1.4.3

--- upload/modules/Store/module.php
+++ upload/modules/Store/module.php
@@ -22,7 +22,7 @@ class Store_Module extends Module {
 
         $name = 'Store';
         $author = '<a href="https://partydragen.com/" target="_blank" rel="nofollow noopener">Partydragen</a>';
-        $module_version = '1.4.2';
+        $module_version = '1.4.3';
         $nameless_version = '2.0.0-pr13';
 
         parent::__construct($this, $name, $author, $module_version, $nameless_version);
