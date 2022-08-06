f971d7d1c5a9c9e4592b49a5f31d3e2ddb699f92
Partydragen <admin@partydragen.com>
2022-08-07 01:28:36 +0200

    Rework list products endpoint

--- upload/modules/Store/includes/endpoints/ListProductsEndpoint.php
+++ upload/modules/Store/includes/endpoints/ListProductsEndpoint.php
@@ -12,27 +12,114 @@ class ListProductsEndpoint extends KeyAuthEndpoint {
         $query = 'SELECT * FROM nl2_store_products';
         $where = ' WHERE deleted = 0';
         $order = ' ORDER BY `order` ASC';
+        $limit = '';
         $params = [];
+
+        if (isset($_GET['product'])) {
+            $where .= ' AND id = ?';
+            array_push($params, $_GET['product']);
+        }
 
         if (isset($_GET['category_id'])) {
             $where .= ' AND category_id = ?';
             array_push($params, $_GET['category_id']);
         }
 
+        if (isset($_GET['limit']) && is_numeric($_GET['limit'])) {
+            $limit .= ' LIMIT '. $_GET['limit'];
+        }
+
         // Ensure the user exists
-        $products_query = $api->getDb()->query($query . $where . $order, $params)->results();
+        $products_query = $api->getDb()->query($query . $where . $order . $limit, $params)->results();
 
         $products_array = [];
-        foreach ($products_query as $product) {
-            $products_array[] = [
-                'id' => (int) $product->id,
-                'category_id' => (int) $product->category_id,
-                'name' => $product->name,
-                'price' => (double) $product->price,
-                'price_cents' => (int) Store::toCents($product->price),
-                'hidden' => (bool) $product->hidden,
-                'disabled' => (bool) $product->disabled
+        foreach ($products_query as $item) {
+            $product = new Product(null, null, $item);
+
+            $global_limit_json = json_decode($product->data()->global_limit, true) ?? [];
+            $user_limit_json = json_decode($product->data()->user_limit, true) ?? [];
+
+            $required_products_list = [];
+            $required_products = json_decode($product->data()->required_products, true) ?? [];
+            foreach ($required_products as $item) {
+                $required_products_list[] = (int) $item;
+            }
+
+            $required_groups_list = [];
+            $required_groups = json_decode($product->data()->required_groups, true) ?? [];
+            foreach ($required_groups as $item) {
+                $required_groups_list[] = (int) $item;
+            }
+
+            $required_integrations_list = [];
+            $required_integrations = json_decode($product->data()->required_integrations, true) ?? [];
+            foreach ($required_integrations as $item) {
+                $required_integrations_list[] = (int) $item;
+            }
+
+            $product_array = [
+                'id' => (int) $product->data()->id,
+                'category_id' => (int) $product->data()->category_id,
+                'name' => $product->data()->name,
+                'price' => (double) $product->data()->price,
+                'price_cents' => (int) Store::toCents($product->data()->price),
+                'hidden' => (bool) $product->data()->hidden,
+                'disabled' => (bool) $product->data()->disabled,
+                'global_limit' => [
+                    'limit' => (int) $global_limit_json['limit'] ?? 0,
+                    'interval' => (int) $global_limit_json['interval'] ?? 1,
+                    'period' => $global_limit_json['period'] ?? 'no_period'
+                ],
+                'user_limit' => [
+                    'limit' => (int) $user_limit_json['limit'] ?? 0,
+                    'interval' => (int) $user_limit_json['interval'] ?? 1,
+                    'period' => $user_limit_json['period'] ?? 'no_period'
+                ],
+                'required_products' => $required_products_list,
+                'required_groups' => $required_groups_list,
+                'required_integrations' => $required_integrations_list
             ];
+
+            // Show description if description param is true
+            if (isset($_GET['description']) && $_GET['description'] == 'true') {
+                $product_array['description'] = $product->data()->description;
+            }
+
+            // List all fields if actions param is true
+            if (isset($_GET['fields']) && $_GET['fields'] == 'true') {
+                $fields = [];
+                foreach ($product->getFields() as $field) {
+                    $fields[] = [
+                        'id' => (int) $field->id,
+                        'identifier' => $field->identifier,
+                        'type' => $field->type,
+                        'required' => (bool) $field->required,
+                        'min' => (int) $field->min,
+                        'min' => (int) $field->min,
+                        'regex' => $field->regex,
+                        'default_value' => $field->default_value
+                    ];
+                }
+                $product_array['fields'] = $fields;
+            }
+
+            // List all actions if actions param is true
+            if (isset($_GET['actions']) && $_GET['actions'] == 'true') {
+                $actions = [];
+                foreach ($product->getActions() as $action) {
+                    $actions[] = [
+                        'id' => (int) $action->data()->id,
+                        'type' => (int) $action->data()->type,
+                        'service_id' => (int) $action->data()->service_id,
+                        'command' => $action->data()->command,
+                        'require_online' => (bool) $action->data()->require_online,
+                        'own_connections' => (bool) $action->data()->own_connections,
+                    ];
+                }
+                $product_array['actions'] = $actions;
+            }
+
+            $products_array[] = $product_array;
         }
  
         $api->returnArray(['products' => $products_array]);
