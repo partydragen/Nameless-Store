2d7708f26d5da0d5e1f5918ddd1da11eba2954f5
partydragen <admin@partydragen.com>
2025-06-29 00:05:21 +0200

    cache user category spending

--- upload/modules/Store/classes/Customer.php
+++ upload/modules/Store/classes/Customer.php
@@ -10,6 +10,7 @@
 class Customer {
 
     private static array $_customers_cache = [];
+    private static array $_customer_spending_cache = [];
 
     private DB $_db;
     private User $_user;
@@ -409,20 +410,22 @@ class Customer {
      * @return int The total amount spent in cents.
      */
     public function calculateSpendingInCategory(int $category_id): int {
-        $spending = $this->_db->query(
-            "SELECT SUM(po.amount_cents) as total
+        return $this->_customer_spending_cache[$category_id] ??= (function ($category_id): int {
+            $spending = $this->_db->query(
+                "SELECT SUM(po.amount_cents) as total
              FROM nl2_store_payments p
              JOIN nl2_store_orders o ON o.id = p.order_id
              JOIN nl2_store_orders_products po ON po.order_id = o.id
              JOIN nl2_store_products pr ON pr.id = po.product_id
              WHERE o.to_customer_id = ? AND pr.category_id = ? AND p.status_id = 1",
-            [$this->data()->id, $category_id]
-        );
+                [$this->data()->id, $category_id]
+            );
 
-        if ($spending->count() && $spending->first()->total) {
-            return (int) $spending->first()->total;
-        }
+            if ($spending->count() && $spending->first()->total) {
+                return (int) $spending->first()->total;
+            }
 
-        return 0;
+            return 0;
+        })($category_id);
     }
 }
